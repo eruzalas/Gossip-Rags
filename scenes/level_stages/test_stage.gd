52175d586a83f5c00117f61e20df61c9710c2ef7
Emily Hocking <104554365@student.swin.edu.au>
2026-03-12 20:49:46 +1100

    Changed directory structure
    Moved Player and NPC into their own folders.

--- scenes/level_stages/test_stage.gd
+++ scenes/level_stages/test_stage.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 # useful for signals https://forum.godotengine.org/t/need-help-understanding-custom-signals-between-objects/50928/2
-const npc_prefab = preload("res://scenes/entities/npc.tscn")
+const npc_prefab = preload("res://scenes/entities/npc/npc.tscn")
 
 func _ready() -> void:
 	# run randomise to ensure all rand calls return more random random randoms
