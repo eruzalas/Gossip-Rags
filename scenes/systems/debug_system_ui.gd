d49fd3136140af88a7bb0fa1632ec61981f5e50a
Jordyn-Reese <161274566+Jordyn-Reese@users.noreply.github.com>
2026-03-27 14:08:41 +1100

    Wrote basic code for sus system

--- scenes/systems/debug_system_ui.gd
+++ scenes/systems/debug_system_ui.gd
@@ -8,11 +8,21 @@ extends Node
 @onready var sus_debug: Label = %"Sus Debug"
 @onready var attention_debug: Label = %"Attention Debug"
 
+#addtional variables --> Will need adjustment with costumes/players and such to demonstrate debug stats
+var sus: float = 0.0
+var multi: float = 1.0
+var steps: int = 0 #changes per second of time spent in suspicion
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# TODO: have debug UI update alongside the sus values
 func _process(delta: float) -> void:
+	pass
+	
+func _determine_sus():
+	
 	pass
--- scenes/systems/sus_meter_system.gd
+++ scenes/systems/sus_meter_system.gd
@@ -1,12 +1,36 @@
-#Script handles the mechanism of suspicion and produces values beacuse of it
+#Script does the calculations and is a node components can hold
 extends Node
 
+#determined the current sus threshold
+var state: String = ""
+var sus_level: float = 0  #int or float?
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+#Calculate suspicion precentage (accounts for costume multipliers and "steps" how long in sus)
+func _calculate_sus(multi: float, sus_precent: float, steps: int):
+	
+	sus_level = (sus_precent + steps) * multi
+	return sus_level
+
+#Calculate current state level and updates state variable
+func _state_level():
+	
+	if(sus_level < 50):
+		state = "calm"
+		print("state: " + state)
+	elif(sus_level > 50 && sus_level < 80):
+		state = "wary"
+		print("state: " + state)
+	elif(sus_level > 80):
+		state = "alert"
+		print("state: " + state)
+		
+	return state
