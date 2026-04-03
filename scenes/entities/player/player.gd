d1798612a1a343f21349d39d3a620e6c92c12dcf
mangeloguy <105336331@student.swin.edu.au>
2026-04-03 12:20:26 +1100

    Rotating closet doors
    not rotating the right way tho, need to add hinge

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -6,6 +6,7 @@ const SPEED = 10.0
 const JUMP_VELOCITY = 10
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
--- scenes/level_stages/test_stage.gd
+++ scenes/level_stages/test_stage.gd
@@ -18,3 +18,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	pass # Replace with function body.
