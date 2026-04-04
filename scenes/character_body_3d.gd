9830b804cf5faa5dde832807849308a952e48b1e
Actronius <113004163+FatalAct@users.noreply.github.com>
2026-04-05 08:12:47 +1000

    friction more or less
    seems a little basic but it's more or less there, values need fine-tuning

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
 
-const SPEED = 10.0
-const JUMP_VELOCITY = 10
+var SPEED = (15 * 1)
+var move_speed = 0 #used to handle speed when running/crouching, inputs required
+var JUMP_VELOCITY = (10 * 1)
+var acceleration = (35 * 1)
+var deceleration = (15 * 1)
 
 
 func _physics_process(delta: float) -> void:
@@ -18,13 +21,20 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
+	if Input.is_action_pressed("run") and !Input.is_action_pressed("crouch"):
+		move_speed = SPEED * 1.5
+	elif Input.is_action_pressed("crouch") and !Input.is_action_pressed("run"):
+		move_speed = SPEED * 0.75
+	else :
+		move_speed = SPEED
 
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		
+		velocity.x = move_toward(velocity.x, direction.x * move_speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, direction.z * move_speed, acceleration * delta)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, deceleration * delta)
+		velocity.z = move_toward(velocity.z, 0, deceleration * delta)
 
 	move_and_slide()
