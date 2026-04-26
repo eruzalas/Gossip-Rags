d450d80b66480b0a4e87a0f72c98bfda6ac5b91a
Jordyn-Reese <161274566+Jordyn-Reese@users.noreply.github.com>
2026-04-26 17:31:41 +1000

    Adjusted attention system
    Made export values for some of the stuff, added multiplier for attention changes

--- scenes/systems/attenion_meter_system.gd
+++ scenes/systems/attenion_meter_system.gd
@@ -9,6 +9,7 @@ var att_level: float = 0  #int or float?
 @export var sus_thresh: int
 
 #Calculate attention to new total attention upon action--mutli=multiplier for costumes, actions=cost of attention action
+#TODO fix maths
 func _calculate_att(multi: float, action: float, current_att: float):
 	att_level = (action * multi) + current_att
 	return att_level
@@ -32,5 +33,5 @@ func _state_level(att_precent: float):
 func _natural_att_decay(att_precent: float):
 	att_level = att_precent
 	#TODO more proper math should be included here following amount of time past since last attention action
-	att_level -= 0.2
+	att_level -= 1
 	return att_level
