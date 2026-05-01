246f421d52ea72c2e921cf2c4e8402a21977e25e
mangeloguy <105336331@student.swin.edu.au>
2026-05-01 14:21:35 +1000

    BABY OIL UPDATE
    - Now disappears when falls
    - Respawns after 3 seconds (for debugging)

--- scenes/entities/interactable/baby_oil.gd
+++ scenes/entities/interactable/baby_oil.gd
@@ -1,26 +1,59 @@
 extends Area3D
 
+#import data
+@export var data: InteractionData
+
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+@onready var sprite: Sprite3D = $Sprite3D
+
 #stores reference to the specific player node
 var current_player: Node3D = null
+var is_active: bool = true 
 
 func _on_body_entered(body: Node3D) -> void:
 	if (body.is_in_group("players")):
 		current_player = body
-		print("Valid Player detected")
-	
 
 func _on_body_exited(body: Node3D) -> void:
 	if (body.is_in_group("players")):
 		current_player = null
 
 func _unhandled_input(event: InputEvent) -> void:
-	if current_player:
+	if is_active and current_player:
 		if (event.is_action_pressed("p1_interact") or event.is_action_pressed("p2_interact")): #makes sure both players can interact
-			print("DEBUG: Interactable emit signal")
-			
-			#pass the curent player and attention value as (5.0)
-			SignalBus.caused_attention.emit(current_player, 5.0)
-			_fall_over()
+			_collect_item() #function to make it fall and disappear smoothly
+
+func _collect_item():
+	if not data:
+		return
+		
+	is_active = false
+	_fall_over()
+	SignalBus.caused_attention.emit(current_player, data.attention_value)
+	
+	await get_tree().create_timer(0.6).timeout #lets the fall over animation finish before object poofs
+	_disappear(false)
+	
+	#starts respawn timer 
+	var timer = get_tree().create_timer(data.respawn_time)
+	timer.timeout.connect(_respawn)
+	
+func _disappear(state: bool):
+	sprite.visible = state
+	collision_shape.set_deferred("disabled", !state) # set_deferred for collision ensures physics thread is ready
+
+func _respawn():
+	#force kills active tweens
+	var tween = create_tween()
+	tween.kill
+	
+	#reset rotations
+	self.rotation_degrees = Vector3.ZERO
+	sprite.rotation_degrees = Vector3.ZERO
+	
+	is_active = true
+	_disappear(true)
+	
 func _fall_over():#really shitty tween animation
 	var tween = create_tween()
 	
--- utils/interaction_data.gd
+++
@@ -1,5 +0,0 @@
-extends Resource
-class_name InteractionData
-
-@export var attention_value: float = 5.0
-@export var respawn_time: float = 5.0
