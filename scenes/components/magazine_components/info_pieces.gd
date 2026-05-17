74bfd4d2c23023380532f9bb6e86834ba4cd54ea
Jordyn-Reese <161274566+Jordyn-Reese@users.noreply.github.com>
2026-05-17 18:20:21 +1000

    Can put in sprites for info in inspector
    Design students can now just add a sprite to each info section in the inspector

--- scenes/components/magazine_components/info_pieces.gd
+++ scenes/components/magazine_components/info_pieces.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
 #variables contained in scene
+
+#portrait
 @onready var info_sprite_2d: Sprite2D = $"Info Sprite2D"
 @onready var info_area_2d: Area2D = $"Info Area2D"
 
+#text
+@onready var text_sprite_2d: Sprite2D = $"Text Sprite2D"
+@onready var text_area_2d: Area2D = $"Text Area2D"
+
 #additional variables
 @export var sprite: Texture2D
+@export var type_info: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	info_sprite_2d.texture = sprite
+	if type_info == 0:
+		info_sprite_2d.texture = sprite
+	elif type_info == 1:
+		text_sprite_2d.texture = sprite
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
