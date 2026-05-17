f13aaffea04b94d142cec3243658b9463135723e
Jordyn-Reese <161274566+Jordyn-Reese@users.noreply.github.com>
2026-05-17 18:54:50 +1000

    fixed errors and merged info
    Fixed variable errors with two variables and merged the info pieces into one component that can handle both types of info (this then can be updated to different types of info)

--- scenes/components/magazine_components/info_pieces.gd
+++ scenes/components/magazine_components/info_pieces.gd
@@ -5,24 +5,38 @@ extends Node2D
 #portrait
 @onready var info_sprite_2d: Sprite2D = $"Info Sprite2D"
 @onready var info_area_2d: Area2D = $"Info Area2D"
+@onready var info_collision_shape_2d: CollisionShape2D = $"Info Area2D/Info CollisionShape2D"
 
 #text
 @onready var text_sprite_2d: Sprite2D = $"Text Sprite2D"
 @onready var text_area_2d: Area2D = $"Text Area2D"
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+@onready var collision_shape_2d: CollisionShape2D = $"Text Area2D/CollisionShape2D"
 
 #additional variables
 @export var sprite: Texture2D
 @export var type_info: int
+var my_group: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if type_info == 0:
-		info_sprite_2d.texture = sprite
-	elif type_info == 1:
+	my_group = self.get_groups()
+	_setup()
+	print(my_group)
+	
+func _setup() -> void:
+	if (my_group.has("text")):
+		info_sprite_2d.queue_free()
+		info_area_2d.process_mode = Node.PROCESS_MODE_DISABLED
+		info_collision_shape_2d.disabled = true
 		text_sprite_2d.texture = sprite
-
-
-
+	elif(my_group.has("pic")):
+		text_sprite_2d.queue_free()
+		text_area_2d.process_mode = Node.PROCESS_MODE_DISABLED
+		rich_text_label.hide()
+		collision_shape_2d.disabled = true
+		info_sprite_2d.texture = sprite
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
